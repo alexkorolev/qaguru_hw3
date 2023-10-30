0d34b940836b69dbd749a5930bd13d74e75235bf
Aleksei Korolev <alexkorolev86@gmail.com>
2023-10-30 22:25:40 +0300

    qa guru hw 3 3 commit

--- src/test/java/DemoQaTests.java
+++ src/test/java/DemoQaTests.java
@@ -8,42 +8,43 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class demoqaTests {
+public class DemoQaTests {
     @BeforeEach
     void beforeMethod(){
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000;
     }
 
     @Test
     void fillThePracticeForm() {
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
-        $("[id=firstName]").setValue("FirstName");
-        $("[id=lastName]").setValue("LastName");
-        $("[id=userEmail]").setValue("test@test.com");
-        $("[id=gender-radio-1]").doubleClick();
-        $("[id=userNumber]").setValue("9990002233");
+        $("#firstName").setValue("FirstName");
+        $("#lastName").setValue("LastName");
+        $("#userEmail").setValue("test@test.com");
+        $("#gender-radio-1").doubleClick();
+        $("#userNumber").setValue("9990002233");
 
-        $("[id=dateOfBirthInput]").click();
+        $("#dateOfBirthInput").click();
         $("select[class*='month-select']").selectOptionByValue("9");
         $("select[class*='year-select']").selectOptionByValue("1985");
         $("div[aria-label*='October 30th']").click();
 
-        $("[id=subjectsContainer] input").click();
-        $("[id=subjectsContainer] input").setValue("English").pressEnter();
+        $("#subjectsContainer input").click();
+        $("#subjectsContainer input").setValue("English").pressEnter();
 
-        $("[id=hobbiesWrapper]").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
 
-        $("[id=uploadPicture]").uploadFile(new File("src/test/data/my.png"));
+        $("#uploadPicture").uploadFromClasspath("my.png");
+        //$("[#uploadPicture]").uploadFile(new File("src/test/data/my.png"));
 
-        $("[id=currentAddress]").setValue("Lenina street 100 , flat 101");
+        $("#currentAddress").setValue("Lenina street 100 , flat 101");
 
-        $("[id=state] input").val("NCR").pressEnter();
-        $("[id=city] input").val("Noida").pressEnter();
+        $("#state input").val("NCR").pressEnter();
+        $("#city input").val("Noida").pressEnter();
         $("#submit").click();
 
         $(".table-responsive").shouldHave(text("FirstName LastName"));
